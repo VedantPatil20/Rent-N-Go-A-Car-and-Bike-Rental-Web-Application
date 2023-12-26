33d52b6021bb598f4a5f3154f668ec231ae80e13
Vedant Patil <vedantnp200@gmail.com>
2023-12-26 18:48:49 +0530

    Admin form UI

---
+++ Rent N Go A Car and Bike Rental Web Application/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rent_N_Go_A_Car_and_Bike_Rental_Web_Application.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
