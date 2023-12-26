590dc5c05b82af048321ee040cbe1e7ff1385e37
Vedant Patil <vedantnp200@gmail.com>
2023-12-26 11:05:58 +0530

    Rent N Go A Car and Bike Rental Web Application created.

---
+++ Rent N Go A Car and Bike Rental Web Application/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Rent_N_Go_A_Car_and_Bike_Rental_Web_Application.Models;
+using System.Diagnostics;
+
+namespace Rent_N_Go_A_Car_and_Bike_Rental_Web_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Rent N Go A Car and Bike Rental Web Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rent_N_Go_A_Car_and_Bike_Rental_Web_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
